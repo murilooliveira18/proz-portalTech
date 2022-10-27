45930cc45018bba6b3469c0a7a29f129c72595ad
Murilo Oliveira <84341631+murilooliveira18@users.noreply.github.com>
2022-10-26 23:12:33 -0300

    Add files via upload

---
+++ proz_codePark_categoriaDeVeiculos.py
@@ -0,0 +1,16 @@
+quantidadeDeRodas = int(input("Digite a quantidade de rodas do veículo:"))
+pesoBruto = float(input("Digite o peso bruto do veículo:"))
+quantidadeDePessoas = int(input("Digite a quantidade de pessoas no veículo:"))
+
+if quantidadeDeRodas >= 2 and quantidadeDeRodas <= 3:
+    print("A melhor categoria de habilitação é a A")
+elif quantidadeDeRodas == 4 and quantidadeDePessoas <= 8 and pesoBruto <= 3500:
+    print("A melhor categoria de habilitação é a B")
+elif quantidadeDeRodas >=4 and pesoBruto >= 3500 and pesoBruto <= 6000:
+     print("A melhor categoria de habilitação é a C")
+elif quantidadeDeRodas >= 4 and quantidadeDePessoas > 8:
+    print("A melhor categoria de habilitação é a D")
+elif quantidadeDeRodas >= 4 and pesoBruto > 6000 :
+    print("A melhor categoria de habilitação é a E")
+else:
+    print("Você digitou dados inválidos ou não tem categoria adequada")
