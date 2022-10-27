9b789822ad4fb0b3fa2cb5d7349dcda59be6c413
Murilo Oliveira <84341631+murilooliveira18@users.noreply.github.com>
2022-10-26 23:22:49 -0300

    Update proz_codePark_mediaDeAlunos.py

--- proz_codePark_mediaDeAlunos.py
+++ proz_codePark_mediaDeAlunos.py
@@ -1,11 +1,16 @@
 nomeAluno = input("Escreva seu nome: ")
 nota1 = float(input("Digite a primeira nota:"))
 nota2 = float(input("Digite a segunda nota:"))
-faltas = float(input("Digite a quantidade de faltas:"))
-media = float(nota1 + nota2 / 2)
+faltas = int(input("Digite a quantidade de faltas:"))
+media = (nota1 + nota2 / 2)
 
-if media <= 7 or faltas > 3: 
-    print(nomeAluno + " você foi reprovado")
+if media < 7 :
+    print(nomeAluno , " você foi reprovado com uma média:" , media)
 else:
-    print(nomeAluno + " você foi aprovado")
+    print(nomeAluno , " você foi aprovado com uma média:" , media)
+
+if faltas >= 3 :
+    print(nomeAluno , " você foi reprovado com um número de faltas:" , faltas)
+else:
+    print(nomeAluno , " você foi aprovado com um número de faltas:" , faltas)
 
